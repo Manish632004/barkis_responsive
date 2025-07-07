545bb47af6ca2d0fff0872a4c3d144811742b86f
Manish632004 <manish632004@gmail.com>
2025-07-07 13:26:14 +0530

    add menu toggle function

---
+++ script.js
@@ -0,0 +1,22 @@
+
+function toggleMenu() {
+    const menu = document.getElementById("mobileMenu");
+    const icon = document.getElementById("menuIcon");
+
+    menu.classList.toggle("active");
+
+    // Toggle icon text
+    if (menu.classList.contains("active")) {
+        icon.textContent = "✖"; // close icon
+    } else {
+        icon.textContent = "☰"; // menu icon
+    }
+}
+
+document.addEventListener('click', function (event) {
+    const menu = document.getElementById('mobileMenu');
+    const toggleBtn = document.querySelector('.menu-toggle');
+    if (!menu.contains(event.target) && !toggleBtn.contains(event.target)) {
+        menu.classList.remove('active');
+    }
+});
