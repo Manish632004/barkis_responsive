7a94c60755b3f446c4f1bf13b45ce65d1f16fd3e
Manish632004 <manish632004@gmail.com>
2025-07-07 17:37:46 +0530

    added id to the nav links

--- script.js
+++ script.js
@@ -13,10 +13,10 @@ function toggleMenu() {
     }
 }
 
-document.addEventListener('click', function (event) {
-    const menu = document.getElementById('mobileMenu');
-    const toggleBtn = document.querySelector('.menu-toggle');
-    if (!menu.contains(event.target) && !toggleBtn.contains(event.target)) {
-        menu.classList.remove('active');
-    }
-});
+// document.addEventListener('click', function (event) {
+//     const menu = document.getElementById('mobileMenu');
+//     const toggleBtn = document.querySelector('.menu-toggle');
+//     if (!menu.contains(event.target) && !toggleBtn.contains(event.target)) {
+//         menu.classList.remove('active');
+//     }
+// });
